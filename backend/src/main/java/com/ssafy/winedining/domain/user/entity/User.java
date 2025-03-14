8c4c7eaa0b4162b9ddf148cf56ff89dedc6eb02e
신동운 <tlsehd5454@naver.com>
2025-03-14 15:11:32 +0900

    feat: User Entity and Service

---
+++ backend/src/main/java/com/ssafy/winedining/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.ssafy.winedining.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
