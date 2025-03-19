98e539b56982273553e38cbd81c5a712da27108c
신동운 <tlsehd5454@naver.com>
2025-03-19 14:48:35 +0900

    [S12P21B202-48] fix: secret key and users

--- backend/src/main/java/com/ssafy/winedining/domain/user/entity/User.java
+++ backend/src/main/java/com/ssafy/winedining/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 public class User {
--- backend/src/main/java/com/ssafy/winedining/global/jwt/JWTUtil.java
+++ backend/src/main/java/com/ssafy/winedining/global/jwt/JWTUtil.java
@@ -8,13 +8,13 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+
 @Component
 public class JWTUtil {
 
     private SecretKey secretKey;
 
-    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-
+    public JWTUtil(@Value("${jwt.secret}")String secret) {
 
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
