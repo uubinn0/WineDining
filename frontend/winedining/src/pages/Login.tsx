4a3e957a965ea7d761b43f19f19b36244da513f0
정다인 <wjdekdls04@naver.com>
2025-03-24 17:00:36 +0900

    [S12P21B202-93] feat: 카카오/구글 로그인 API 연결 완료

--- frontend/winedining/src/pages/Login.tsx
+++ frontend/winedining/src/pages/Login.tsx
@@ -1,13 +1,46 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { assert } from "console";
 
 function Login() {
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
   const navigate = useNavigate();
+
+  const handleKakaoLogin = async () => {
+    try {
+      //로컬 스토리에 kakao 상태값 저장
+      localStorage.setItem("provider", "KAKAO");
+      console.log("provider (KAKAO):", localStorage.getItem("provider"));
+
+      window.location.href = "https://j12b202.p.ssafy.io/api/v1/auth/oauth2/authorization/kakao"; // 해당 URL로 리다이렉션
+    } catch (error) {
+      console.error("카카오 로그인 요청 실패:", error);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      //로컬 스토리에 google 상태값 저장
+      localStorage.setItem("provider", "GOOGLE");
+      console.log("provider (GOOGLE):", localStorage.getItem("provider"));
+      // 백엔드에서 구글 인증 URL 가져오기
+      const response = await axios.get("https://j12b202.p.ssafy.io/api/v1/auth/oauth2/authorization/google");
+      const googleAuthUrl = response.data.data.link; // 백엔드가 반환하는 인증 URL
+      window.location.href = googleAuthUrl; // 해당 URL로 리다이렉션
+    } catch (error) {
+      console.error("구글 로그인 요청 실패:", error);
+    }
+  };
 
   return (
     <div style={styles.container}>
-      <h3 style={styles.button}>카카오 로그인</h3>
-      <h3 style={styles.button}>구글로 시작하기</h3>
+      <h3 style={styles.button} onClick={handleKakaoLogin}>
+        카카오 로그인
+      </h3>
+      <h3 style={styles.button} onClick={handleGoogleLogin}>
+        구글로 시작하기
+      </h3>
       <button style={styles.button} onClick={() => navigate("/home")}>
         홈으로 이동하기
       </button>
