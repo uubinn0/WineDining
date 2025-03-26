81b94015cbce2a80495aaecb33f36dcb70284959
나유빈 <ub.devl0908@gmail.com>
2025-03-26 16:11:18 +0900

    [#S12P21B202-107]feat: 와인 벡터 모델 추가

--- fastapi/app/db/models/wine.py
+++ fastapi/app/db/models/wine.py
@@ -1,6 +1,8 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import VECTOR  # pgvector를 위한 타입
 
 class Wine(SQLModel, table=True):
     __tablename__ = "wines"  # 명시적 선언 (옵션)
@@ -24,3 +26,12 @@ class Wine(SQLModel, table=True):
     type_id: int
     wine_group_id: int
     year: Optional[str] = None
+
+
+class WineVector(SQLModel, table=True):
+    __tablename__ = "wine_vectors"  # 명시적 선언 (옵션)
+
+    wine_id: int = Field(primary_key=True, foreign_key="wines.id")
+    feature_vector: Optional[list[float]] = Field(
+        sa_column=Column(VECTOR)  # PostgreSQL vector 타입 지정
+    )
