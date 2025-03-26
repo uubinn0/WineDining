6e7f6c9230c9505d3449130d44bf047db74cf0a0
신동운 <tlsehd5454@naver.com>
2025-03-26 17:32:42 +0900

    [S12P21B202-121] feat: 와인 위시리스트 기능 구현

---
+++ backend/src/main/java/com/ssafy/winedining/domain/collection/dto/WishResponseDTO.java
@@ -0,0 +1,16 @@
+package com.ssafy.winedining.domain.collection.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WishResponseDTO {
+    private Long id;
+    private String createdAt;
+    private WineDTO wine;
+}
