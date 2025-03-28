a522775c3cb49eb783edd2df1ca3cb99219d07c2
신동운 <tlsehd5454@naver.com>
2025-03-28 16:37:13 +0900

    chore: init fast api

--- backend/src/main/java/com/ssafy/winedining/domain/preference/repository/PreferenceRepository.java
+++ backend/src/main/java/com/ssafy/winedining/domain/preference/repository/PreferenceRepository.java
@@ -3,11 +3,12 @@ package com.ssafy.winedining.domain.preference.repository;
 import com.ssafy.winedining.domain.preference.entity.Preference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PreferenceRepository extends JpaRepository<Preference, Long> {
 
     // UserId를 이용해 추천 테이블 조회
     Optional<Preference> findByUserId(Long userId);
-    Optional<Preference> findFirstByUserIdOrderByUpdatedAtDesc(Long userId);
+    List<Preference> findAllByUserId(Long userId);
 }
