5ac0d3c8ac05caf49b19e344a133608de388c738
신동운 <tlsehd5454@naver.com>
2025-03-28 11:01:37 +0900

    [S12P21B202-136] fix: preference naming

--- backend/src/main/java/com/ssafy/winedining/domain/preference/controller/PreferenceController.java
+++ backend/src/main/java/com/ssafy/winedining/domain/preference/controller/PreferenceController.java
@@ -3,7 +3,10 @@ package com.ssafy.winedining.domain.preference.controller;
 import com.ssafy.winedining.domain.preference.service.PreferenceService;
 import com.ssafy.winedining.domain.preference.dto.request.PreferenceTestRequest;
 import com.ssafy.winedining.global.auth.dto.CustomOAuth2User;
+import com.ssafy.winedining.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,13 +27,20 @@ public class PreferenceController {
      * @return
      */
     @PostMapping("/test")
-    public String getPreferenceTest(
+    public ResponseEntity<ApiResponse<Void>> getPreferenceTest(
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
             @RequestBody PreferenceTestRequest request){
         // 유저 컨텍스트에서 유저ID 받기
         Long userId = customOAuth2User.getUserId();customOAuth2User.getUserId();
         preferenceService.saveUserPreferenceTest(userId, request);
-        return "저장 완료";
-    }
 
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .status(HttpStatus.OK.value())
+                .success(true)
+                .message("취향 추천 검사 결과 저장 성공")
+                .data(null)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 }
