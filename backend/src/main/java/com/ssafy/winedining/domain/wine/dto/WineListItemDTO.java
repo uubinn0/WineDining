efdf4a9e9dff46c931dad1f780f99780db90d2e5
신동운 <tlsehd5454@naver.com>
2025-04-07 15:57:38 +0900

    hotfix: winelist typeName to type

--- backend/src/main/java/com/ssafy/winedining/domain/wine/dto/WineListItemDTO.java
+++ backend/src/main/java/com/ssafy/winedining/domain/wine/dto/WineListItemDTO.java
@@ -7,7 +7,7 @@ public class WineListItemDTO {
     private Long wineId;
     private String name;
     private String image;
-    private String typeName;
+    private String type;
     private String country;
     private String grape;
     private boolean isWish;
