fd8251a04e76d5f1870550c5acc7b284b26b8a5f
신동운 <tlsehd5454@naver.com>
2025-04-07 16:45:19 +0900

    hotfix: change wish item repository

--- backend/src/main/java/com/ssafy/winedining/domain/collection/repository/WishItemRepository.java
+++ backend/src/main/java/com/ssafy/winedining/domain/collection/repository/WishItemRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.winedining.domain.collection.repository;
 
 import com.ssafy.winedining.domain.collection.entity.WishItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +17,6 @@ public interface WishItemRepository extends JpaRepository<WishItem, Long> {
     void deleteByUserIdAndWineId(Long userId, Long wineId);
     boolean existsByUserIdAndWineId(Long userId, Long wineId);
     void deleteByUserId(Long userId);
-    Set<Long> findWineIdsByUserId(Long userId);
+    @Query("SELECT w.wine.id FROM WishItem w WHERE w.user.id = :userId")
+    Set<Long> findWineIdsByUserId(@Param("userId") Long userId);
 }
